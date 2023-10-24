df69d980363302b5542d8a6f528ab68aa57c379a
Geraldo Homero <geraldohomero@protonmail.com>
2023-10-24 13:48:02 +0000

    Atualizar o remapkeys_ShiftR_ccedilla.sh

--- remapkeys_ShiftR_ccedilla.sh
+++ remapkeys_ShiftR_ccedilla.sh
@@ -1,9 +1,7 @@
+sleep 30 &&
 xmodmap -e "keycode 62 = Mode_switch" && 
 xmodmap -e "keycode 34 = dead_acute dead_grave Up" && 
 xmodmap -e "keycode 61 = semicolon colon Down" && 
 xmodmap -e "keycode 47 = ccedilla Ccedilla Left" && 
 xmodmap -e "keycode 48 = dead_tilde dead_circumflex Right" &&
 xmodmap -e "keycode 9 = Escape apostrophe quotedbl"
-
-
-
